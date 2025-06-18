1086c8588567d79d90bac685bc10cf20e77fc220
TatKuv <ttatiana.s.mails@gmail.com>
2025-06-18 22:13:25 +0400

    Initial Commit

---
+++ TodoList (TestTask)/TodoList__TestTask_App.swift
@@ -0,0 +1,17 @@
+//
+//  TodoList__TestTask_App.swift
+//  TodoList (TestTask)
+//
+//  Created by Tatiana Kuvarzina on 18.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct TodoList__TestTask_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
