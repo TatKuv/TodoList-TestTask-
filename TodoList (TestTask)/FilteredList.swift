5b3465fe29269d30a0dcd63c70c3d5cb7ff8aa15
TatKuv <ttatiana.s.mails@gmail.com>
2025-06-21 16:55:13 +0400

    Filterring with Featchrequest

---
+++ TodoList (TestTask)/FilteredList.swift
@@ -0,0 +1,18 @@
+//
+//  FilteredList.swift
+//  TodoList (TestTask)
+//
+//  Created by Tatiana Kuvarzina on 21.06.2025.
+//
+
+import SwiftUI
+
+struct FilteredList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FilteredList()
+}
